9cade61372e8aa17214a3fbc809fcfcc7ab10cc8
StrukovOI <strukov.oi@phystech.edu>
2024-09-22 16:58:44 +0300

    Add files via upload

---
+++ ДЗ на 27.09.2024/2.py
@@ -0,0 +1,14 @@
+def funktion ():
+    a=int(input('Введите число, которое необходимо разложить на простые множители \n'))
+    z=[]
+    s = f'{a} ='
+    while a!=1:
+        for i in range(2, a+1):
+            if a%i==0:
+                z.append(i)
+                a=a//i
+                break
+    for i in range(len(z)):
+        s = f'{s} {z[i]} *'
+    return s[:-1]
+print(funktion())
